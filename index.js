0204c4a307a831c66743c684e30492d175631c59
Camara Vamoro <cvamorocamson@gmail.com>
2023-08-17 07:49:03 +0100

    someModif

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let menuInfo = document.querySelector(".menuActive");
 let menuHam = document.querySelector(".menu");
 let title = document.querySelector(".title");
+let body = document.querySelector(".body")
 
 
 menuHam.onclick= ()=>{
     menuInfo.classList.toggle("active")
     menuHam.classList.toggle("active")
     title.classList.toggle("active")
+}
+
+body.onclick = ()=>{
+    menuInfo.classList.remove("active")
+    menuHam.classList.remove("active")
 }
 
